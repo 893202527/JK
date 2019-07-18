0f1966681046f19fe93399f7b94ba631f1eb822d
893202527 <893202527@qq.com>
2019-07-18 17:40:48 +0800

    1.ModelSerializer 序列化

--- MyDjango/xycDemo/views.py
+++ MyDjango/xycDemo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse
 import json
 from . import models
-
+from rest_framework import status
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from .serializer import  UserSerializer
@@ -46,24 +46,31 @@ class Userview(APIView):
         user=models.User.objects.all()
         serializer=UserSerializer(user,many=True)
         return Response(serializer.data)
-    def register_user(requsert):
-        if requsert.method == 'POST' and requsert.POST:
-            phoneNumber = requsert.POST.get('phoneNumber')
-            if models.User.objects.filter(phoneNumber=phoneNumber) != None:
-                nickName = requsert.POST.get('nickname')
-                username = requsert.POST.get('username')
-                sex = requsert.POST.get('sex')
-                age = requsert.POST.get('age')
-                password = requsert.POST.get('password')
-
-                user = models.User(nickName=nickName, password=password, username=username, sex=sex, age=age,
-                                   phoneNumber=phoneNumber)
-                user.save()
-                msg = {'msg': 'successful', 'msg_code': '1'}
-                return HttpResponse(json.dumps(msg), content_type='application/json;charset=utf-8')
-            else:
-                msg = {'msg': '已注册', 'msg_code': '1001'}
-                return HttpResponse(json.dumps(msg), content_type='application/json;charset=utf-8')
+    def post(self,request,format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            msg = {'msg': '请求方式错误', 'msg_code': '2002'}
-            return HttpResponse(json.dumps(msg), content_type='application/json;charset=utf-8')
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def register_user(requsert):
+    #     if requsert.method == 'POST' and requsert.POST:
+    #         phoneNumber = requsert.POST.get('phoneNumber')
+    #         if models.User.objects.filter(phoneNumber=phoneNumber) != None:
+    #             nickName = requsert.POST.get('nickname')
+    #             username = requsert.POST.get('username')
+    #             sex = requsert.POST.get('sex')
+    #             age = requsert.POST.get('age')
+    #             password = requsert.POST.get('password')
+    #
+    #             user = models.User(nickName=nickName, password=password, username=username, sex=sex, age=age,
+    #                                phoneNumber=phoneNumber)
+    #             user.save()
+    #             msg = {'msg': 'successful', 'msg_code': '1'}
+    #             return HttpResponse(json.dumps(msg), content_type='application/json;charset=utf-8')
+    #         else:
+    #             msg = {'msg': '已注册', 'msg_code': '1001'}
+    #             return HttpResponse(json.dumps(msg), content_type='application/json;charset=utf-8')
+    #     else:
+    #         msg = {'msg': '请求方式错误', 'msg_code': '2002'}
+    #         return HttpResponse(json.dumps(msg), content_type='application/json;charset=utf-8')
